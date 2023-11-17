819fb95806316e704d47ad939ea6b7e0dd473088
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2023-11-17 10:46:07 -0300

    Added some documentation in batchs

--- build-release.bat
+++ build-release.bat
@@ -2,6 +2,11 @@
 cmake -S . -B build
 cd build
 
+:: Pega o primeiro arquivo .sln (que é o único que interessa) e compila usando o msbuild
+:: [cmake in Windows] https://stackoverflow.com/questions/9878225/using-cmake-on-windows-for-c
+:: [% vs %% in batch] https://jeffpar.github.io/kbarchive/kb/075/Q75634/
+:: [% vs %% in batch] https://stackoverflow.com/questions/14509652/what-is-the-difference-between-and-in-a-cmd-file
+
 for %%f in (./*.sln) do (
      if exist %%f (
         echo:
